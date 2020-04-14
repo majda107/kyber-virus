a7be07f660c094ce792072d43b9116742eaef830
majda107 <majda.trpkos@gmail.com>
2020-04-14 23:23:31 +0200

    Colors

--- src/components/CountryStats.vue
+++ src/components/CountryStats.vue
@@ -6,15 +6,15 @@
         <span class="stats-cases-header">Cases</span>
 
         <span class="stats-cases-label">Today</span>
-        <span class="stats-cases-value">{{ stats.cases.new }}</span>
+        <span class="stats-cases-value new">{{ stats.cases.new }}</span>
         <span class="stats-cases-label">Active</span>
-        <span class="stats-cases-value">{{ stats.cases.active }}</span>
+        <span class="stats-cases-value active">{{ stats.cases.active }}</span>
         <span class="stats-cases-label">Critical</span>
-        <span class="stats-cases-value">{{ stats.cases.critical }}</span>
+        <span class="stats-cases-value critical">{{ stats.cases.critical }}</span>
         <span class="stats-cases-label">Recovered</span>
-        <span class="stats-cases-value">{{ stats.cases.recovered }}</span>
+        <span class="stats-cases-value recovered">{{ stats.cases.recovered }}</span>
         <span class="stats-cases-label">Total</span>
-        <span class="stats-cases-value">{{ stats.cases.total }}</span>
+        <span class="stats-cases-value total">{{ stats.cases.total }}</span>
       </div>
       <div class="stats-deaths">
         <span class="stats-deaths-header">Deaths</span>
@@ -46,54 +46,68 @@ export default {
 <style lang="sass" scoped>
 @import '../../sass/abstracts/variables'
 
-.container
-    // background-color: $dark-color
-    border-radius: 10px
-
 .stats
+  display: grid
+  padding: 20px
+  // justify-content: center
+  grid-template-areas: 'name name' 'cases deaths' 'cases tests'
+  grid-template-rows: auto auto auto
+  column-gap: 80px
+  row-gap: 40px
+
+  &-name
+    font-size: 1.9rem
+    grid-area: name
+
+  &-cases, &-deaths, &-tests
     display: grid
-    padding: 20px
-    // justify-content: center
-    grid-template-areas: 'name name' 'cases deaths' 'cases tests' 
-    grid-template-rows: auto auto auto
-    column-gap: 80px
-    row-gap: 40px
+    grid-template-columns: 1fr 1fr
+    row-gap: 3px
 
-    &-name
-        font-size: 1.9rem
-        grid-area: name
+    &-header
+      grid-column-start: 1
+      grid-column-end: 3
+      justify-self: flex-start
+      font-size: 1.7rem
+      font-weight: 400
 
-    &-cases, &-deaths, &-tests
-        display: grid
-        grid-template-columns: 1fr 1fr
-        row-gap: 3px
+    &-label
+      justify-self: flex-start
+      font-weight: 100
+      font-size: 1.1rem
 
-        &-header
-            grid-column-start: 1
-            grid-column-end: 3
-            justify-self: flex-start
-            font-size: 1.7rem
-            font-weight: 400
+    &-value
+      justify-self: flex-end
+      font-weight: 500
+      color: $primary-color
+      font-size: 1.1rem
 
-        &-label
-            justify-self: flex-start
-            font-weight: 100
-            font-size: 1.1rem
+  &-cases
+    grid-area: cases
 
-        &-value
-            justify-self: flex-end
-            font-weight: 500
-            color: $primary-color
-            font-size: 1.1rem
+  &-deaths
+    grid-area: deaths
 
-    &-cases
-        grid-area: cases
+  &-tests
+    grid-area: tests
 
-    &-deaths
-        grid-area: deaths
-    
-    &-tests
-        grid-area: tests
+        // ses", color: "rgba(70, 200, 70)", fill: false , additional: [{ path: "deaths.total", label: "Total deaths", color: "rgba(200, 70, 70)", fill: false }] },
+        // { path: "cases.new", label: "New cases", color: "rgba(255,140,0, 0.4)" },
+        // { path: "tests.total", label: "Total tests", color: "rgba(50, 50, 250, 0.3)" },
+        // { path: "cases.critical", label: "Critical cases", color: "rgba(250, 50, 50, 0.3)"
+.new
+
+.active
+  color: rgba(210, 210, 210, 0.7)
+
+.critical
+  color: rgba(250, 70, 70, 0.85)
+
+.recovered
+  color: rgba(120, 250, 120, 0.6)
+
+.total
+  color: rgba(120, 120, 250, 0.8)
 
 @media screen and ( max-width: 600px )
   .stats
@@ -102,6 +116,5 @@ export default {
     justify-content: center
     // padding-left: 20px
     // padding-right: 60px
-
 </style>
 
